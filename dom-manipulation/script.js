ed000d7cfdfe8d6665bbdfa9ec15c8bd82ae6bdc
Omar Albaz <omaralbaz635@gmail.com>
2024-10-20 16:09:07 +0300

    creat : dom-manipulation

---
+++ dom-manipulation/script.js
@@ -0,0 +1,11 @@
+const quote = [
+    {text : "The best way to predict the future is to create it" , category : "Inspiration" } ,
+    {text : "TLife is 10% what happens to us and 90% how we react to it" , category : "Motivation" } ,
+    {text : "Your time is limited, don't waste it living someone else's life" , category : "Wisdom" } ,
+]
+const showRandomQuote = function () {
+    
+}
+const createAddQuoteForm = function () {
+
+}
