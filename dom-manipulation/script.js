1c2f83c7d390173115c90e13f2a07b3e1d9a732a
Omar Albaz <omaralbaz635@gmail.com>
2024-10-20 16:26:36 +0300

    creat : dom-manipulation

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -14,11 +14,21 @@ function createAddQuoteForm() {
     const newQuoteCategory = document.getElementById('newQuoteCategory').value;
     if (newQuoteText && newQuoteCategory) {
         quotes.push({ text: newQuoteText, category: newQuoteCategory });
+        const quoteDisplay = document.getElementById('quoteDisplay');
+        quoteDisplay.innerHTML = '';
+        const quoteTextElement = document.createElement('p');
+        quoteTextElement.textContent = newQuoteText;
+        const quoteCategoryElement = document.createElement('p');
+        quoteCategoryElement.textContent = newQuoteCategory;
+        quoteCategoryElement.style.fontStyle = 'italic';
+        quoteDisplay.appendChild(quoteTextElement);
+        quoteDisplay.appendChild(quoteCategoryElement);
         document.getElementById('newQuoteText').value = '';
         document.getElementById('newQuoteCategory').value = '';
-        alert("New quote added successfully!");
-    } else {
-      alert("Please enter both the quote and category.");
+        alert('New quote added successfully!');
+    } 
+    else {
+        alert('Please enter both the quote and category.');
     }
-  }
+}
 document.getElementById('newQuote').addEventListener('click', showRandomQuote);
